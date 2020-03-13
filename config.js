07e9c8e4f229b7b82e1ec312432fc246f85b5ed6
sattaya037 <sattaya.ra@ics-th.com>
2020-03-13 09:45:54 +0700

    make it better

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    channelAccessToken: 'U9A88cpmj1xH+gSi6f4REAEQiqoF7f2x46QAH+LqDg4EKvWmkOkL00YLdd/oT01IBrAnatoIy1agnaKdSKEOnC9lpDwKc4x9IUh6yNeguknThsIVJddyk8E1cCQugBQum20/VsAZHp/0DQPEZFWeeQdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'fbc2d97950065d60ea924670f5c4f286'
+  };
+  
+  module.exports = config;
---
+++ server.js
@@ -0,0 +1,81 @@
+
+const express = require('express');
+const app = express();
+const line = require('@line/bot-sdk');
+const config = require('./config');
+const client = new line.Client(config);
+const port = process.env.PORT || 5000;
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
+});
+
+function handleEvent(event) {
+  switch (event.type) {
+    case 'message':
+      switch (event.message.type) {
+        case 'text':
+          return   Intent(event);
+        case 'image':
+          return  image(event);
+        case 'video':
+          return  broadcast(event);
+        case 'audio':
+          return  broadcast(event);
+        case 'location':
+          return  broadcast(event);
+        case 'sticker':
+          return  broadcast(event);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+
+    case 'follow':
+      return follow(event);
+
+    case 'unfollow':
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+    case 'join':
+      return replyText(event.replyToken, `Joined ${event.source.type}`);
+
+    case 'leave':
+      return console.log(`Left: ${JSON.stringify(event)}`);
+
+    case 'postback':
+
+      return replyText(event.replyToken, `Got postback: ${data}`);
+
+    case 'beacon':
+      const dm = `${Buffer.from(event.beacon.dm || '', 'hex').toString('utf8')}`;
+      return beacon(event,dm);
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+  }
+}
+
+function image(event){
+  var messageID=event.message.id;
+}
+
+function Intent(event){
+    console.log(event)
+}
+
+
+function follow(event){
+  var msg={
+    type: 'text',
+    text:  "ขอบคุณที่เป็นเพื่อนกับเรา \uDBC0\uDC03  หากไม่ต้องการรับการแจ้งเตือน ขอให้ไปที่เมนูที่มุมขวาบนของหน้าจอแล้วปิดใช้งานการแจ้งเตือน \uDBC0\uDC2E   "
+  };
+  client.replyMessage(event.replyToken, msg);
+}
+
+function beacon(event,dm){ 
+}
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
